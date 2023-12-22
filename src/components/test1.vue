7ff4ddcb9a3f1e30dbc59e6d7dd09ceb5b893532
nhniuhua <2602755527@qq.com>
2023-12-22 09:43:43 +0800

    修改app

---
+++ src/components/test1.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>22222</div>
+</template>
+
+<script>
+export default {
+  name: 'testPage',
+  data() {
+    return {
+      num: 1
+    }
+  }
+}
+</script>
+
+<style>
+</style>
